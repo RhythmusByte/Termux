f46c4600deaef2640b1f54e0eba7067f4a36a9ad
RhythmusByte <rhythmusbyte@proton.me>
2023-10-06 13:29:32 +0530

    Program to print a triangle followed by square

---
+++ Rough/tri-sqr.py
@@ -0,0 +1,16 @@
+def main ():
+  # To print a triangle
+  pront('   *')
+  print('  ***')
+  print(' *****')
+  print('*******')
+
+  # To print a blank line
+
+  print()
+
+  # To print a square
+  print('* * * *')
+  print('* * * *')
+  print('* * * *')
+  print('* * * *')
