d6e8b5aed02340055af97f93c8d4159b47683ac6
Akhil Mahesh <akhilmahesh@proton.me>
2023-11-14 10:31:54 +0530

    Update rough.py

--- Rough/rough.py
+++ Rough/rough.py
@@ -7,3 +7,10 @@ print(a[::-1])  # Reverse a string || gnizama si nohtyP
 print(a[-2:-5]) # Gives an empty string
 print(a[0:]) # Python is amazing
 print(a[:17]) # Python is amazing
+
+# del(a) || Deleting a string
+print(a*2)
+print('y' in a) # Checking if the character 'y' is in the string
+print('o' not in a)
+
+print('%s is my %d st preference' %("Python",1))
