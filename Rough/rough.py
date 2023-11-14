de03c72c66ee9b1cbafd5f612eac63e76e4f22e2
Akhil Mahesh <akhilmahesh@proton.me>
2023-11-14 11:06:34 +0530

    Update rough.py

--- Rough/rough.py
+++ Rough/rough.py
@@ -15,3 +15,58 @@ print('y' in a) # Checking if the character 'y' is in the string
 print('o' not in a)
 
 print('%s is my %d st preference' %("Python",1))
+
+# Some String functions
+
+print(len(a))
+print(a.capitalize()) # Capitalize first letter
+print(a.title())
+print(a.count("is")) # Checking how many 'is' is available in the string
+print(a.find("is")) 
+print(a.index("is"))
+
+# Lowercasing the string
+
+print(a.casefold())
+print(a.lower())
+
+print(a.islower())
+
+print(a.startswith("T"))
+print(a.partition("is"))
+print(a.strip("*"))
+print(a.replace('amazing','fantastic'))
+print(a.split(';',1))
+print("{} and {} are alphabets".format("a","b"))
+
+
+
+
+# Output
+# Python is amazing
+# y
+# g
+# gnizama si nohtyP
+# 
+# azi
+# Python is amazing
+# Python is amazing
+# Python is amazingPython is amazing
+# True
+# False
+# Python is my 1 st preference
+# 17
+# Python is amazing
+# Python Is Amazing
+# 1
+# 7
+# 7
+# python is amazing
+# python is amazing
+# False
+# False
+# ('Python ', 'is', ' amazing')
+# Python is amazing
+# Python is fantastic
+# ['Python is amazing']
+# a and b are alphabets
