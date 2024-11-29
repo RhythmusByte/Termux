8a7eaddcef60d444856112971fb42bf5619a418d
Akhil Mahesh <akhilmahesh@proton.me>
2024-11-29 20:44:30 +0530

    Quick Update

--- bashFiles/greet.sh
+++ bashFiles/greet.sh
@@ -5,12 +5,14 @@ userName="rhythmusbyte"
 up=$(uptime -p)
 date=$(date +%d-%m-%Y)
 
-echo "|---------------------------|"
-echo "|-       Hello $name       -|"
-echo "|-   Welcome to Termux :)  -|"
-echo "|---------------------------|"
-echo "|-   User: $userName    -|"
-echo "|-   Date: $date      -|"
-# echo "|-   Uptime: $up"
-echo "|---------------------------|"
+echo "┌─-------------------------─┐"
+echo "│        Hello $name        │"
+echo "│    Welcome to Termux :)   │"
+echo "├─--------------------------┤"
+echo "│    User: $userName     │"
+echo "│    Date: $date       │"
+echo "└───────────────────────────┘"
 echo " "
+
+# echo "|-   Uptime: $up"
+
